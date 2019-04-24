2134666dddb7ae2a3f898c40fd7883d907017956
Christopher Nugent <awedeven+github@gmail.com>
2019-04-24 01:39:41 -0700

    Update script.py

--- script.py
+++ script.py
@@ -4,9 +4,6 @@ And be one traveler, long I stood
 And looked down one as far as I could
 To where it bent in the undergrowth;"""
 
-# Convert the poem to lowercase, since computers are case sensitive.
-poem = poem.lower()
-
 # Split the poem into words and sort those alphabetically
 words = sorted(poem.split())
 
